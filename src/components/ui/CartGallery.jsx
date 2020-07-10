dc5472ff145e5b748b20554a32eaaaccffc8f2c6
tetiana <tetianayaremko@gmail.com>
2020-07-10 16:29:52 +0200

    increments product

--- src/components/ui/CartGallery.jsx
+++ src/components/ui/CartGallery.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import {Button, Col, Image, Row} from "react-bootstrap";
+import {MdKeyboardArrowUp} from "react-icons/md";
 
 
 function CartGallery(props) {
 
-    const {data, removeFromCart} = props
+    const {data, removeFromCart, addToCart} = props
     console.log(data)
     return (
        <>
-                {data.map(product => <div  className={'d-flex flex-column text-center justify-content-center align-items-center'}>
+                {data.map(product => <div  className={'d-flex flex-column  justify-content-between align-items-center'}>
                     <>
                         <button type="button" className="close align-self-end" aria-label="Close" onClick={(id)=>removeFromCart(product.productId._id)} >
                             <span aria-hidden="true">&times;</span>
                         </button>
-                        <Image src={product.productId.imageUrl} fluid />
-                        <span>{product.productId.name}</span>
-                        <span>{product.productId.description}</span>
-                        <span>{product.productId.price}</span>
-                        <span>{product.quantity}</span>
+                        <Row className={'d-flex  text-center justify-content-between align-items-center'}>
+                            <Col className={'d-flex flex-column text-center justify-content-between align-items-center'} >
+                                <Image src={product.productId.imageUrl} fluid />
+                                <span>{product.productId.name}</span>
+                                <span>{product.productId.description}</span>
+                                <span>{product.productId.price}</span>
+                                <span>{product.quantity}</span>
+                            </Col>
+                            <Col>
+                                <MdKeyboardArrowUp onClick={(id)=>addToCart(product.productId._id)} />
+                            </Col>
+                        </Row>
                     </>
                 </div>)}
         </>
